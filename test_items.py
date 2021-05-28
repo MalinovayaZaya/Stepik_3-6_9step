1b66b54a96c39b47d02b24026fd857c7a16940b2
Polina <32522035+MalinovayaZaya@users.noreply.github.com>
2021-05-28 21:12:30 +0300

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_page_contains_add_to_cart_button_with_assert(browser):
     browser.get(link)
-    time.sleep(3) # при необходимости можно раскоментить :В
+    time.sleep(30) # при необходимости можно раскоментить :В
 
     cart = browser.find_elements_by_css_selector("button.btn-add-to-basket")
     assert cart != 0, "No such elements on page"
